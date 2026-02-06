982a4802ea6f93665d8e545df3d835d755a3b1ff
lastvoidtemplar <deyandelchev03@abv.bg>
2026-02-06 02:18:29 +0200

    better transposition table size

--- src/main.rs
+++ src/main.rs
@@ -20,7 +20,7 @@ mod positions;
 mod solvers;
 mod transposition_table;
 
-const TRANSPOSITION_TABLE_SIZE: usize = 8 * 1024 * 1024;
+const TRANSPOSITION_TABLE_SIZE: usize = 8388593;
 
 fn select_board_and_solver(encoded_position: &str) -> Box<dyn Solver> {
     let mut solver_arg: Option<String> = None;
